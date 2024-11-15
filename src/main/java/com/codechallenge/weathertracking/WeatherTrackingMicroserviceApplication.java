27a9fae2bb17318866c1ca354e48353dc313efd9
samira <samira.radmaneshfar@gmail.com>
2024-11-16 00:27:41 +0100

    init commit

---
+++ src/main/java/com/codechallenge/weathertracking/WeatherTrackingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.weathertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTrackingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTrackingMicroserviceApplication.class, args);
+	}
+
+}
